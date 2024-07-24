d83ca80826334dd0408d1e6e7a2914f79497fcff
Concombre843 <93040513+Concombre843@users.noreply.github.com>
2024-07-24 19:28:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let accessToken = '';
 
 // Fonction pour gérer l'authentification
 function handleAuth() {
-    const clientId = '23PGTQ'; // Remplacez par votre vrai Client ID Fitbit
-    const redirectUri = encodeURIComponent('https://Concombre843.github.io/callback.html');
+    const clientId = 'VOTRE_CLIENT_ID'; // Remplacez par votre vrai Client ID Fitbit
+    const redirectUri = encodeURIComponent('https://[votre-nom-utilisateur].github.io/callback.html');
     const scope = encodeURIComponent('sleep profile');
     const authUrl = `https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
     window.location.href = authUrl;
